b936d988f9273b525d0e199308500c4034bd9f78
KhuseinovRuslan <huseko21@gmail.com>
2024-12-18 15:11:12 +0300

    Moved workibng code from one file to anonther

--- add_delete_buttons.py
+++ add_delete_buttons.py
@@ -4,27 +4,12 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-
-app.layout = dbc.Container([
-    html.H1('Главный заголовко страницы'),
-    html.Div([
-    html.Button("Добавить поле", id='add-button', n_clicks=0),
+app.layout = html.Div([
+    html.Button("Добавить поля для долготы и широты", id='add-button', n_clicks=0),
     html.Button("Удалить последнее поле", id='remove-button', n_clicks=0),
-    
-    ]),
-
-    dbc.Row([
-        dbc.Col([
-            html.Div(id='input-container'),
-            dcc.Store(id='clicks-store', data={'add': 0, 'remove': 0, 'remove_was': 0, 'add_clics_was': 0})  # Храним значения кликов
-        ]),
-        dbc.Col([
-            dbc.CardHeader('Введите Долготу начальной точки'),
-
-        ])         
-    ]) 
+    html.Div(id='input-container'),
+    dcc.Store(id='clicks-store', data={'add': 0, 'remove': 0, 'remove_was': 0, 'add_clics_was': 0})  # Храним значения кликов
 ])
-
 
 @app.callback(
     Output('input-container', 'children'),
@@ -39,17 +24,34 @@ def update_inputs(add_clicks, remove_clicks, current_inputs, clics_data):
     if current_inputs is None:
         current_inputs = []
 
-    # Логика добавления поля
+    # Логика добавления полей
     if add_clicks > clics_data['add_clics_was']:
         clics_data['add_clics_was'] = add_clicks
-        new_input = dbc.Input(
+        
+        # Создаем два поля: одно для долготы, другое для широты
+        longitude_input = dbc.Input(
             type='number',
-            placeholder='Введите долготу промежуточной точки',
-            id={'type': 'dynamic-input', 'index': add_clicks},
+            placeholder='Введите долготу',
+            id={'type': 'longitude-input', 'index': add_clicks},
             valid=False,
             invalid=False
         )
-        current_inputs.append(new_input)
+        
+        latitude_input = dbc.Input(
+            type='number',
+            placeholder='Введите широту',
+            id={'type': 'latitude-input', 'index': add_clicks},
+            valid=False,
+            invalid=False
+        )
+        
+        # Добавляем их в одну строку с двумя колонками
+        current_inputs.append(
+            dbc.Row([
+                dbc.Col(longitude_input, width=6),
+                dbc.Col(latitude_input, width=6)
+            ])
+        )
 
     # Логика удаления поля
     if remove_clicks > clics_data['remove_was'] and current_inputs:
@@ -59,32 +61,55 @@ def update_inputs(add_clicks, remove_clicks, current_inputs, clics_data):
     return current_inputs, clics_data  # Возвращаем обновленные данные
 
 @app.callback(
-    Output({'type': 'dynamic-input', 'index': dash.dependencies.ALL}, 'valid'),
-    Output({'type': 'dynamic-input', 'index': dash.dependencies.ALL}, 'invalid'),
-    Input({'type': 'dynamic-input', 'index': dash.dependencies.ALL}, 'value')
+    Output({'type': 'longitude-input', 'index': dash.dependencies.ALL}, 'valid'),
+    Output({'type': 'longitude-input', 'index': dash.dependencies.ALL}, 'invalid'),
+    Output({'type': 'latitude-input', 'index': dash.dependencies.ALL}, 'valid'),
+    Output({'type': 'latitude-input', 'index': dash.dependencies.ALL}, 'invalid'),
+    Input({'type': 'longitude-input', 'index': dash.dependencies.ALL}, 'value'),
+    Input({'type': 'latitude-input', 'index': dash.dependencies.ALL}, 'value')
 )
-def validate_inputs(values):
-    valid_states = []
-    invalid_states = []
+def validate_inputs(longitude_values, latitude_values):
+    valid_longitude_states = []
+    invalid_longitude_states = []
+    valid_latitude_states = []
+    invalid_latitude_states = []
     
-    for value in values:
+    for value in longitude_values:
         if value is None:
-            valid_states.append(False)
-            invalid_states.append(False)
+            valid_longitude_states.append(False)
+            invalid_longitude_states.append(False)
         else:
             try:
                 num_value = float(value)
                 if -180 <= num_value <= 180:
-                    valid_states.append(True)
-                    invalid_states.append(False)
+                    valid_longitude_states.append(True)
+                    invalid_longitude_states.append(False)
                 else:
-                    valid_states.append(False)
-                    invalid_states.append(True)
+                    valid_longitude_states.append(False)
+                    invalid_longitude_states.append(True)
             except ValueError:
-                valid_states.append(False)
-                invalid_states.append(True)
+                valid_longitude_states.append(False)
+                invalid_longitude_states.append(True)
 
-    return valid_states, invalid_states
+    for value in latitude_values:
+        if value is None:
+            valid_latitude_states.append(False)
+            invalid_latitude_states.append(False)
+        else:
+            try:
+                num_value = float(value)
+                if -90 <= num_value <= 90:
+                    valid_latitude_states.append(True)
+                    invalid_latitude_states.append(False)
+                else:
+                    valid_latitude_states.append(False)
+                    invalid_latitude_states.append(True)
+            except ValueError:
+                valid_latitude_states.append(False)
+                invalid_latitude_states.append(True)
+
+    return valid_longitude_states, invalid_longitude_states, valid_latitude_states, invalid_latitude_states
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
